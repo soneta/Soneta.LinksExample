ed0e57b3dce118ca50940f4680543890c4208189
Klaudiusz Bryja <klaudiusz.bryja@enova.pl>
2017-05-04 16:09:45 +0200

    Step 2

--- LinkSampleAddIn/LinkCreatorWorker.cs
+++ LinkSampleAddIn/LinkCreatorWorker.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using LinkSampleAddIn;
 using Soneta.Business;
+using Soneta.Core;
 using Soneta.Core.DbTuples;
 
 [assembly: Worker(typeof(LinkCreatorWorker), typeof(DbTuple))]
@@ -21,11 +22,36 @@ namespace LinkSampleAddIn
         )]
         public void CreateAndSend()
         {
+            var content = Tuple.Fields["Content"]?.ToString();
+            if (string.IsNullOrWhiteSpace(content)) return;
+
+            var link = CreateLink();
+            SendLink(link, content);
         }
+
+        private string CreateLink()
+        {
+            using (var tran = Tuple.Session.Logout(true))
+            {
+                var pars = Tuple.Fields["Pars"]?.ToString() ?? "Nie określono parametrów";
+                var info = Tuple.Fields["Info"]?.ToString() ?? "Nie określono pola Info";
+                var link = HTTPLinkInfo.Create(Tuple, pars).GetLink();
+
+                tran.Commit();
+
+                return $"<a href=\"{link}\" target=\"_blank\" title=\"{info}\">{info}</a>";
+            }
+        }
+
+        private void SendLink
+            (string link, string content) => MailSender.Send($"{content}<br/>{link}", "klaudiusz.bryja@enova.pl", Tuple.Session);
 
         public static bool IsVisibleCreateAndSend
             (DbTuple tuple) => tuple?.Definicja
-                                   .Fields.Any(field => field.Name == "TrescEmail") ?? false;
+                                   .Fields.Where(field => field.Name == "Content"
+                                                          || field.Name == "Info"
+                                                          || field.Name == "Pars")
+                                   .Count() == 3;
 
     }
 
