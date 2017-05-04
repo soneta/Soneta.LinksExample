4e203379d0e4f3f976bbf4f00270b189df4d6022
Klaudiusz Bryja <klaudiusz.bryja@enova.pl>
2017-05-04 16:13:20 +0200

    Step 4

--- LinkSampleAddIn/LinkHandler.cs
+++ LinkSampleAddIn/LinkHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Soneta.Business;
+using Soneta.Business.UI;
 using Soneta.Core;
 
 namespace LinkSampleAddIn
@@ -8,10 +9,9 @@ namespace LinkSampleAddIn
     internal class LinkHandler : HTTPLinkInfo.IHTTPLinkAction
     {
 
-        object HTTPLinkInfo.IHTTPLinkAction.Invoke(IGuidedRow document, string pars)
-        {
-            throw new NotImplementedException();
-        }
+        object HTTPLinkInfo.IHTTPLinkAction.Invoke
+            (IGuidedRow document, string pars) => new MessageBoxInformation("Zlinkuj się z enova365",
+            $"Wywołanie link'ów enova365{Environment.NewLine}Dokument: {document}{Environment.NewLine}Parametry: {pars}");
 
     }
 
